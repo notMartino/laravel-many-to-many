d3d663f371220699a5cf3776793569e44a38b15c
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-06-01 16:27:37 +0200

    Implementazione header con dettagli

--- laravel-main/resources/views/layouts/main-layout.blade.php
+++ laravel-main/resources/views/layouts/main-layout.blade.php
@@ -7,8 +7,14 @@
     <title>Many to Many</title>
 
     <script src="https://kit.fontawesome.com/2e0cc00556.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
 </head>
 <body>
+    @include('components.header')
+
     @yield('content')
 </body>
 </html>
--- laravel-main/routes/web.php
+++ laravel-main/routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'EmployeeController@EmployeeView');
+Route::get('/', 'EmployeeController@EmployeeView') -> name('employeeLink');
