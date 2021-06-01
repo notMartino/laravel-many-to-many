34a91f2b206d587eaa153bcde455becc87e9b22f
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-06-01 15:05:43 +0200

    Creazione tabelle, con colonne vuote, su DB

---
+++ laravel-main/resources/views/pages/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main-layout')
+
+@section('content')
+    <main>
+        <h2>
+            Location 
+            {{-- <i class="fas fa-chevron-left"></i> --}}
+            <i class="fas fa-angle-double-left"></i>
+            <i class="fas fa-grip-lines"></i>
+            1
+            <i class="fas fa-grip-lines"></i>
+            N
+            <i class="fas fa-grip-lines"></i>
+            <i class="fas fa-angle-double-right"></i>
+            Employee
+            <i class="fas fa-angle-double-left"></i>
+            <i class="fas fa-grip-lines"></i>
+            N
+            <i class="fas fa-grip-lines"></i>
+            N
+            <i class="fas fa-grip-lines"></i>
+            <i class="fas fa-angle-double-right"></i>
+            Task
+        </h2>
+    </main>
+@endsection
